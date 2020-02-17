637645a295d29fcc757c8e2e222358d7742fcc22
zq <zq13408401721@139.com>
2020-02-17 16:02:14 +0800

    添加适配器Adapter

---
+++ app/src/main/java/com/shop/base/BaseAdapter.java
@@ -0,0 +1,115 @@
+package com.shop.base;
+
+import android.content.Context;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public abstract class BaseAdapter<T> extends RecyclerView.Adapter {
+
+    ItemClickHandler itemClickHandler;
+
+    protected List<T> mDatas;
+    protected Context mContext;
+
+    public BaseAdapter(List<T> data, Context context){
+        mDatas = data;
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(getLayout(),null);
+        BaseViewHolder holder = new BaseViewHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (itemClickHandler != null){
+                    itemClickHandler.itemClick(holder.getLayoutPosition(),holder);
+                }
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        T t = mDatas.get(position);
+        bindData((BaseViewHolder) holder,t);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+
+    /**
+     * 刷新替换所有列表数据
+     * @param list
+     */
+    public void updata(List<T> list){
+        mDatas.clear();
+        mDatas.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    /**
+     * 分页加载数据的刷新
+     */
+    public void refreshList(List<T> list){
+        mDatas.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    public abstract int getLayout();
+
+    //hodler是对应的item的管理的，T当前item所对应的数据
+    public abstract void bindData(BaseViewHolder holder,T t);
+
+    /**
+     * 创建基类的ViewHolder
+     */
+    public static class BaseViewHolder extends RecyclerView.ViewHolder{
+
+        private SparseArray items;
+
+        public BaseViewHolder(@NonNull View itemView) {
+            super(itemView);
+            items = new SparseArray();
+        }
+
+        /**
+         * 获取item中的元素
+         * @param id
+         * @return
+         */
+        public View getView(int id){
+            View view = (View) items.get(id);
+            if(view == null){
+                view = itemView.findViewById(id);
+                items.append(id,view);
+            }
+            return view;
+        }
+    }
+
+    //设置回调接口的监听
+    public void setOnItemClickHandler(ItemClickHandler clickHandler){
+        this.itemClickHandler = clickHandler;
+    }
+
+    /**
+     * 定义回调接口
+     */
+    public interface ItemClickHandler{
+        void itemClick(int position, BaseViewHolder holder);
+    }
+
+}
--- app/src/main/java/com/shop/ui/home/HomeFragment.java
+++ app/src/main/java/com/shop/ui/home/HomeFragment.java
@@ -1,14 +1,34 @@
 package com.shop.ui.home;
 
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.shop.R;
+import com.shop.base.BaseAdapter;
 import com.shop.base.BaseFragment;
-import com.shop.base.BasePersenter;
-import com.shop.interfaces.IBasePersenter;
 import com.shop.interfaces.home.HomeConstract;
 import com.shop.models.bean.IndexBean;
-import com.shop.persenter.home.*;
+import com.shop.persenter.home.HomePersenter;
 
-public class HomeFragment extends BaseFragment<HomeConstract.Persenter> implements HomeConstract.View {
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
+public class HomeFragment extends BaseFragment<HomeConstract.Persenter> implements HomeConstract.View, BaseAdapter.ItemClickHandler {
+
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+    BrandAdapter brandAdapter;
+    List<IndexBean.DataBean.BrandListBean> list;
+
+
+
     @Override
     protected int getLayout() {
         return R.layout.fragment_home;
@@ -16,7 +36,11 @@ public class HomeFragment extends BaseFragment<HomeConstract.Persenter> implemen
 
     @Override
     protected void initView() {
-
+        list = new ArrayList<>();
+        brandAdapter = new BrandAdapter(list,context);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(brandAdapter);
+        brandAdapter.setOnItemClickHandler(this);
     }
 
     @Override
@@ -31,6 +55,19 @@ public class HomeFragment extends BaseFragment<HomeConstract.Persenter> implemen
 
     @Override
     public void getHomeDataReturn(IndexBean result) {
+        //刷新Brand列表数据
+        brandAdapter.updata(result.getData().getBrandList());
+    }
 
+    /**
+     * 接口回调的方法
+     * @param position
+     * @param holder
+     */
+    @Override
+    public void itemClick(int position, BaseAdapter.BaseViewHolder holder) {
+        IndexBean.DataBean.BrandListBean bean = list.get(position);
+        ((TextView)holder.getView(R.id.txt_name)).setText(bean.getName()+"新的名字");
+        Log.i("brand-click",String.valueOf(position));
     }
 }
